61cba9cb263b251abfd30f497cf701777088e69a
yuri noriki <yurinoriki@hotmail.com>
2025-10-09 11:01:46 -0300

    app de genero

--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
 
-def hello_view(request):
-    return JsonResponse({
-        'message': 'Hello word',
-        'id': '5',
-        'genero':'drama'
-        })
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_view),
+   
 ]
