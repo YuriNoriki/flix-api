2c2b1013c965048c831dbcf6f2f75a581a4b23ca
yuri noriki <yurinoriki@hotmail.com>
2025-11-14 01:59:38 -0300

    app reviews e validators

--- movies/admin.py
+++ movies/admin.py
@@ -4,6 +4,7 @@ from movies.models import Movie
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = (
-        'title', 'genre', 'realese_date',
-        'actors', 'resume',
+        'id', 'title', 'genre', 
+        'realese_date',
+        'resume',
     )
