be46edc4a3bd1839e6bfc90bab884f7fc5127594
yuri noriki <yurinoriki@hotmail.com>
2026-02-13 00:33:07 -0300

    Serializer dinamicos e flake8

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,49 +1,35 @@
 from rest_framework import serializers
-from movies.models import Movie
 from django.db.models import Avg
+from actors.serializers import ActorSerializer
+from genres.serializers import GenreSerializer
+from movies.models import Movie
+
 
 class MovieSerializer(serializers.ModelSerializer):
-
-    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
 
-    # campos de validação
-    def validate_realese_data(self, value):
-        if value.year < 1900:
-            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990')
-        return value
-    
-    def validate_resume(self, value):
-        if len(value) > 500:
-            raise serializers.ValidationError('O resumo não pode ser mais de 500 caracteres')
-        return value
-    
-    # campo calculado 
+
+class MovieListDetailSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True)
+    genre = GenreSerializer()
+    rate = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'genre', 'actors', 'realese_date', 'rate', 'resume']
+
     def get_rate(self, obj):
+        average_rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
 
-        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        if average_rate:
+            return round(average_rate, 1)
 
-        if rate:
-            return rate
-        
-        return None
-        
-        # reviews é o nome da ligação no model de reviews 
-        #reviews = obj.reviews.all()
-        
-        #if reviews:
-            #sum_reviews = 0
 
-            # É AQUI que o QuerySet é avaliado e os dados SÃO carregados:
-            #for review in reviews:
-                # É AQUI que o campo 'stars' de cada objeto 'review' é acessado:
-                #sum_reviews += review.stars
-
-            #reviews_count = reviews.count()
-
-            #return round(sum_reviews / reviews_count, 1)
-        
-        #return None
+class MovieStatsSerializer(serializers.Serializer):
+    total_movies = serializers.IntegerField()
+    movies_by_genre = serializers.ListField()
+    total_reviews = serializers.IntegerField()
+    average_stars = serializers.FloatField()
--- movies/views.py
+++ movies/views.py
@@ -1,43 +1,52 @@
+from django.db.models import Count, Avg
 from rest_framework import generics, views, response, status
-from movies.models import Movie
-from movies.serializers import MovieSerializer
 from rest_framework.permissions import IsAuthenticated
 from app.permission import GlobalDefaultPermission
-from django.db.models import Count, Avg
+from movies.models import Movie
+from movies.serializers import MovieSerializer, MovieListDetailSerializer, MovieStatsSerializer
 from reviews.models import Review
 
+
 class MovieCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,GlobalDefaultPermission)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
 
-class MovieRetrieverUpdateDestroy(generics.RetrieveDestroyAPIView):
-    permission_classes = (IsAuthenticated,GlobalDefaultPermission)
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer
+        return MovieSerializer
+
+
+class MovieRetrieverUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
 
-# Define a classe de visualização para estatísticas de filmes usando a base do Django Rest Framework
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer
+        return MovieSerializer
+
+
 class MovieStatsView(views.APIView):
-    #Define as classes de permissão: usuário autenticado e permissões globais do sistema
-     permission_classes = (IsAuthenticated,GlobalDefaultPermission)
-     # Define o conjunto de dados base (todos os filmes) para as consultas
-     queryset = Movie.objects.all()
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
 
-    # Define o método que lidará com as requisições do tipo GET
-     def get(self, request):
-        # Conta a quantidade total de filmes registrados no banco de dados
-         total_movies = self.queryset.count()
-         # Agrupa filmes por nome de gênero e conta quantos filmes existem em cada um
-         movies_by_genre = self.queryset.values('genre__name').annotate(count = Count('id'))
-         # Conta o total de avaliações registradas na tabela Review
-         total_reviews = Review.objects.count()
-         # Calcula a média aritmética de todas as notas (estrelas) dadas nas avaliações
-         average_stars = Review.objects.aggregate(avg_stars = Avg('stars'))['avg_stars']
-         
-         # Retorna uma resposta JSON com os dados processados e o status HTTP 200 (OK)
-         return response.Response(data={
+    def get(self, request):
+        total_movies = self.queryset.count()
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        total_reviews = Review.objects.count()
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
+
+        data = {
             'total_movies': total_movies,
             'movies_by_genre': movies_by_genre,
             'total_reviews': total_reviews,
             'average_stars': round(average_stars, 1) if average_stars else 0,
-         }, status=status.HTTP_200_OK)
+        }
+        serializer = MovieStatsSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        return response.Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK,
+        )
